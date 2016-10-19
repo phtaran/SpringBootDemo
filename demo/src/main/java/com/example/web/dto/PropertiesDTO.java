26bec93254006e66024ab1fdf30773dea6723f2b
pavel taranov <plap@softserveinc.com>
2016-10-19 17:31:57 +0300

    Test getting values from application.properties

---
+++ demo/src/main/java/com/example/web/dto/PropertiesDTO.java
@@ -0,0 +1,16 @@
+package com.example.web.dto;
+
+import java.io.Serializable;
+
+public class PropertiesDTO implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private final String firstProperty;
+    private final String secondProperty;
+
+    public PropertiesDTO(String firstProperty, String secondProperty) {
+        this.firstProperty = firstProperty;
+        this.secondProperty = secondProperty;
+    }
+}
---
+++ demo/src/main/java/com/example/web/resource/HomeResource.java
@@ -0,0 +1,45 @@
+package com.example.web.resource;
+
+import static org.springframework.http.ResponseEntity.ok;
+
+import java.util.Map;
+
+import javax.inject.Inject;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import com.example.web.dto.PropertiesDTO;
+
+@RestController("/")
+@PropertySource(value="classpath:application.properties")
+public class HomeResource {
+
+    @Value("${first_property}")
+    String firstProperty;
+
+    @Inject
+    Environment environment;
+
+    @RequestMapping(method = RequestMethod.GET)
+    String getHomePage() {
+        return "home";
+    }
+
+    @RequestMapping(value = "property",
+                    method = RequestMethod.GET,
+                    produces = MediaType.APPLICATION_JSON_VALUE)
+    PropertiesDTO getProperty() {
+
+        String secondProperty = environment.getProperty("second_property");
+        PropertiesDTO propertiesDTO = new PropertiesDTO(firstProperty, secondProperty);
+
+        ResponseEntity<PropertiesDTO> responseEntity = ResponseEntity.ok(propertiesDTO);
+        return propertiesDTO;
+    }
+}
