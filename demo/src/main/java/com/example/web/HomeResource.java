6951f1eb23e4771b6c3ba2df30ea5c040b58279c
pavel taranov <plap@softserveinc.com>
2016-10-19 15:20:05 +0300

    Add first rest resource

---
+++ demo/src/main/java/com/example/web/HomeResource.java
@@ -0,0 +1,14 @@
+package com.example.web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/")
+public class HomeResource {
+
+    @RequestMapping(method = RequestMethod.GET)
+    String getHomePage() {
+        return "home";
+    }
+}
