7141da365b4d5f77d537923e168886d1c1e21f97
phtaran <phtaran@gmail.com>
2016-11-19 19:47:18 +0200

    Add unit test to see changes on jenkins (#1)
    * Add unit test to see changes on jenkins
    
    * Add failed test
    
    * jenkins
    
    * jenkins

---
+++ src/test/java/com/example/SimpleUnitTest.java
@@ -0,0 +1,18 @@
+package com.example;
+
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.fail;
+
+
+public class SimpleUnitTest {
+
+    @Test
+    public void testJenkins() {
+        fail("fail for test jenkins");
+    }
+
+    
+}
