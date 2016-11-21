92b5e8a2061368eb9d57797d38dbd3158b542e02
phtaran <differenceN2@yandex.ru>
2016-11-21 19:04:16 +0200

    Don't configure transaction manager in DatabaseConfig
    Comment creating of transaction manager, because it cause problem with transactions in tests
    To know about transactions in integration tests see http://docs.spring.io/spring/docs/current/spring-framework-reference/html/integration-testing.html#testcontext-tx

--- src/test/java/com/example/web/resource/PersonResourceTest.java
+++ src/test/java/com/example/web/resource/PersonResourceTest.java
@@ -1,27 +1,42 @@
 package com.example.web.resource;
 
+import com.example.DemoApplication;
+import com.example.domain.Person;
+import com.example.repository.PersonRepository;
+import com.example.service.PersonService;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.context.WebApplicationContext;
 
+import javax.inject.Inject;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@RunWith(SpringRunner.class)
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = { DemoApplication.class })
+@Transactional
 @WebAppConfiguration
 public class PersonResourceTest {
 
     @Autowired
     private WebApplicationContext webApplicationContext;
+
+    @Inject
+    private PersonRepository personRepository;
+
+    @Inject PersonService personService;
 
     private MockMvc mockMvc;
 
@@ -33,10 +48,16 @@ public class PersonResourceTest {
     @Test
     public void getPersonById() throws Exception {
 
-        this.mockMvc.perform(get("/accounts/1").accept(MediaType.parseMediaType("application/json;charset=UTF-8")))
+        Person person = new Person("John", 20, "test");
+        personRepository.saveAndFlush(person);
+        System.out.println(personRepository.findAll());
+
+        this.mockMvc.perform(get("/person/{id}", person.getId()).accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType("application/json"))
-                .andExpect(jsonPath("$.name").value("Lee"));
+                .andDo(print())
+                .andExpect(jsonPath("$.name").value("John"))
+                .andExpect(jsonPath("$.age").value(20))
+                .andExpect(jsonPath("$.content").value("test"));
     }
 
 }
