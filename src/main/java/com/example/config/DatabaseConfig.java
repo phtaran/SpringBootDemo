d116d5c76d9bad2c5927805b7a8af5d92cb507c6
phtaran <differenceN2@yandex.ru>
2016-12-06 19:51:50 +0200

    Add liquibase into the project

--- src/main/java/com/example/config/DatabaseConfig.java
+++ src/main/java/com/example/config/DatabaseConfig.java
@@ -1,8 +1,15 @@
 package com.example.config;
 
+import liquibase.integration.spring.SpringLiquibase;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.liquibase.LiquibaseProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -12,6 +19,7 @@ import javax.sql.DataSource;
 @Configuration
 @EnableJpaRepositories("com.example.repository")
 @EnableTransactionManagement
+@PropertySource("classpath:liquibase.properties")
 public class DatabaseConfig {
 
     @Value("${spring.datasource.url}")
@@ -23,8 +31,21 @@ public class DatabaseConfig {
     @Value("${spring.datasource.password}")
     String datasourcePassword;
 
+    @Autowired
+    Environment env;
+
     @Bean
-    public DataSource dataSource() {
+    public DataSource getDataSource() {
         return new DriverManagerDataSource(datasourceUrl, datasourceName, datasourcePassword);
+    }
+
+    @Bean
+    public SpringLiquibase liquibase() {
+        SpringLiquibase liquibase = new SpringLiquibase();
+        liquibase.setChangeLog(env.getProperty("changeLogFile"));
+        liquibase.setContexts(env.getProperty("contexts"));
+        liquibase.setDataSource(this.getDataSource());
+        liquibase.setDropFirst(Boolean.parseBoolean(env.getProperty("dropFirst")));
+        return liquibase;
     }
 }
